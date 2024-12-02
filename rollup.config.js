bc3640c24f9b081e8a13cbc52e2ea4c81831f1eb
Niko Pastore <nikopastore@gmail.com>
2024-12-01 20:40:36 -0800

    asd

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,36 @@
-import svelte from 'rollup-plugin-svelte';
+import svelte from '@rollup/plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-import terser from '@rollup/plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js', // Output in public/build
-  },
-  plugins: [
-    svelte({
-      compilerOptions: {
-        dev: !production,
-      },
-      emitCss: true,
-    }),
-    css({ output: 'bundle.css' }), // CSS output
-    resolve({
-      browser: true,
-      dedupe: ['svelte'],
-    }),
-    commonjs(),
-    !production &&
-      livereload({
-        watch: 'public', // Watch the public directory
-        port: 35731,
-      }),
-    production && terser(),
-  ],
-  watch: {
-    clearScreen: false,
-  },
+    input: 'src/main.js',
+    output: {
+        sourcemap: true,
+        format: 'iife',
+        name: 'app',
+        file: 'public/build/bundle.js',
+    },
+    plugins: [
+        svelte({
+            compilerOptions: {
+                dev: !production,
+            },
+        }),
+        css({ output: 'bundle.css' }),
+        resolve({
+            browser: true,
+            dedupe: ['svelte'],
+        }),
+        commonjs(),
+        !production && livereload('public'),
+        production && terser(),
+    ],
+    watch: {
+        clearScreen: false,
+    },
 };
