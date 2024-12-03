014f22cda2e9806a33a97055617c5ecfdbd9ff35
Niko Pastore <nikopastore@gmail.com>
2024-12-03 14:37:26 -0800

    Update chart.svelte

--- src/chart.svelte
+++ src/chart.svelte
@@ -14,8 +14,12 @@
     }
 
     function drawChart() {
+        console.log("Drawing Chart...");
+
+        // Remove previous chart content
         d3.select(chart).selectAll("*").remove();
 
+        // Dimensions
         const margin = { top: 20, right: 20, bottom: 50, left: 50 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
@@ -28,6 +32,7 @@
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
+        // Scales
         const x = d3
             .scaleTime()
             .domain(d3.extent(filteredData, (d) => new Date(d.Date)))
@@ -42,51 +47,19 @@
         svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x));
         svg.append("g").call(d3.axisLeft(y));
 
-        // Line path for Monday
-        const mondayData = filteredData.filter((d) => d.Day === "Monday");
-
-        let cumulativeInvestment = 0;
-        const cumulativeData = mondayData.map((d) => {
-            cumulativeInvestment += investmentAmount;
-            return { Date: d.Date, Value: cumulativeInvestment };
-        });
+        // Add Line
+        const line = d3
+            .line()
+            .x((d) => x(new Date(d.Date)))
+            .y((d) => y(d.Close * investmentAmount));
 
         svg
             .append("path")
-            .datum(cumulativeData)
+            .datum(filteredData)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
-            .attr("stroke-width", 2)
-            .attr(
-                "d",
-                d3
-                    .line()
-                    .x((d) => x(new Date(d.Date)))
-                    .y((d) => y(d.Value))
-            );
-
-        // Hover interaction
-        const focus = svg.append("g").style("display", "none");
-
-        focus.append("line").attr("class", "hover-line").attr("y1", 0).attr("y2", height).style("stroke", "black");
-
-        svg
-            .append("rect")
-            .attr("width", width)
-            .attr("height", height)
-            .style("fill", "none")
-            .style("pointer-events", "all")
-            .on("mouseover", () => focus.style("display", null))
-            .on("mousemove", (event) => {
-                const bisectDate = d3.bisector((d) => new Date(d.Date)).left;
-                const x0 = x.invert(d3.pointer(event)[0]);
-                const i = bisectDate(cumulativeData, x0, 1);
-                const d = cumulativeData[i - 1];
-
-                focus.attr("transform", `translate(${x(new Date(d.Date))},0)`);
-                console.log("Hover Data:", d);
-            })
-            .on("mouseout", () => focus.style("display", "none"));
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
     }
 </script>
 
