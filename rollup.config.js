f2025482b660be52c8bf1b86e7d8715404fac033
Niko Pastore <nikopastore@gmail.com>
2024-12-13 22:11:05 -0800

    Fix deployment paths

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
-import terser from '@rollup/plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
