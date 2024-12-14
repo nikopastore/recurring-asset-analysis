3804e6a53feec33fa314766eb01037256820707f
Niko Pastore <nikopastore@gmail.com>
2024-12-13 21:11:36 -0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
 import terser from '@rollup/plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -13,26 +14,31 @@ export default {
     sourcemap: true,
     format: 'iife',
     name: 'app',
-    file: 'public/build/bundle.js',
+    file: 'public/build/bundle.js',  // Ensure this matches your public folder setup
   },
   plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+        '%BASE%': production ? '/recurring-asset-analysis' : '',  // Update base path for GitHub Pages
+      },
+    }),
     svelte({
       compilerOptions: {
         dev: !production,
       },
       emitCss: true,
     }),
-    css({ output: 'bundle.css' }),
+    css({ output: 'bundle.css' }),  // Output CSS bundle
     resolve({
       browser: true,
       dedupe: ['svelte'],
     }),
     commonjs(),
-    !production &&
-      livereload({
-        watch: 'public',
-        port: 35731,
-      }),
+    !production && livereload({
+      watch: 'public',
+      port: 35731,
+    }),
     production && terser(),
   ],
   watch: {
